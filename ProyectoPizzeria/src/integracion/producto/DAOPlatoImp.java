f8481fc92dc0a28dcee45b6d4ae2e463f051c2e5
danie <danie@LAPTOP-4PPNFQ3D>
2023-04-22 16:29:40 +0200

    He actualizado integracion.producto

---
+++ ProyectoPizzeria/src/integracion/producto/DAOPlatoImp.java
@@ -0,0 +1,31 @@
+package integracion.producto;
+
+import negocio.producto.TPlato;
+
+public class DAOPlatoImp implements DAOPlato {
+
+	@Override
+	public String insertaPlato(TPlato tp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean daDeBajaPlato(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TPlato obtenPlato(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean modificaPlato(TPlato tp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
