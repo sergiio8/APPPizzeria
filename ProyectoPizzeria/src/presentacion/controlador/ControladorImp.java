d5e0b28afad9b91d5789acc84f9061af5b8f0cb9
aparreno14 <aparreno@ucm.es>
2023-04-24 20:28:53 +0200

    He añadido json al classpath para poder importar, y ya leo y escribo JSON

--- ProyectoPizzeria/src/presentacion/controlador/ControladorImp.java
+++ ProyectoPizzeria/src/presentacion/controlador/ControladorImp.java
@@ -1,12 +1,23 @@
 package presentacion.controlador;
 
+import negocio.factoria.FactoriaAbstractaNegocio;
+import negocio.mesas.SAMesas;
+import negocio.mesas.TMesas;
 import presentacion.Evento;
 
 public class ControladorImp extends Controlador { //implementacion
 
 	@Override
 	public void accion(Evento e, Object datos) {
-		// TODO Auto-generated method stub
+		switch(e) {
+		case ALTA_MESA:
+			TMesas tm = (TMesas) datos;
+			SAMesas saMesas = FactoriaAbstractaNegocio.getInstace().crearSAMesas();
+			
+			int res = saMesas.alta(tm);
+			
+			
+		}
 
 	}
 
