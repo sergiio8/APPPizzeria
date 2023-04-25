c5e4bf4c95b36c2351bf5ce5e57d5b3c6fd0cdef
danie <danie@LAPTOP-4PPNFQ3D>
2023-04-25 11:12:56 +0200

    ModificarPlatoVista hecha

--- ProyectoPizzeria/src/presentacion/producto/ModificarPlatoVista.java
+++ ProyectoPizzeria/src/presentacion/producto/ModificarPlatoVista.java
@@ -1,5 +1,108 @@
 package presentacion.producto;
 
-public class ModificarPlatoVista {
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Frame;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import presentacion.Evento;
+import presentacion.IGUI;
+
+public class ModificarPlatoVista extends JDialog implements IGUI {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ModificarPlatoVista(Frame parent) {
+		super(parent, true);
+		initGUI();
+	}
+
+	private void initGUI() {
+		setTitle("Modificar plato");
+		setLayout(new BorderLayout());
+		
+		//ID
+		JPanel panel1 = new JPanel(new FlowLayout());
+		JLabel ID = new JLabel("ID_plato: ");
+		JTextField textID = new JTextField();
+		
+		panel1.add(ID);
+		panel1.add(textID);
+		
+		add(panel1, BorderLayout.CENTER);
+		
+		//Nombre
+		JPanel panel2 = new JPanel(new FlowLayout());
+		JLabel cantidad = new JLabel("Nombre: ");
+		JTextField textCantidad = new JTextField();
+		
+		panel2.add(cantidad);
+		panel2.add(textCantidad);
+		
+		add(panel2, BorderLayout.CENTER);
+		
+		//Precio
+		JPanel panel3 = new JPanel(new FlowLayout());
+		JLabel precio = new JLabel("Precio: ");
+		JTextField textPrecio = new JTextField();
+		
+		panel3.add(precio);
+		panel3.add(textPrecio);
+		
+		add(panel3, BorderLayout.CENTER);
+		
+		//Ingredientes
+		JPanel panel4 = new JPanel(new FlowLayout());
+		JLabel ingredientes = new JLabel("Ingredientes: ");
+		JTextField textIngredientes = new JTextField();
+		
+		panel4.add(ingredientes);
+		panel4.add(textIngredientes);
+		
+		add(panel4, BorderLayout.CENTER);
+
+		//Descripcion
+		JPanel panel5 = new JPanel(new FlowLayout());
+		JLabel descripcion = new JLabel("Descripcion: ");
+		JTextField textDescripcion = new JTextField();
+		
+		panel5.add(descripcion);
+		panel5.add(textDescripcion);
+		
+		add(panel5, BorderLayout.CENTER);
+		
+		//Stock??
+		
+		JPanel endPanel = new JPanel(new FlowLayout());
+		
+		JButton modificar = new JButton("Modificar");
+		modificar.addActionListener((e) -> modificar());
+		JButton cancelar = new JButton("Cancelar");
+	    cancelar.addActionListener((e) -> cancelar());
+	    endPanel.add(modificar);
+	    endPanel.add(cancelar);
+		
+		add(panel3, BorderLayout.CENTER);
+	}
+	
+	private void modificar() {
+		
+		
+	}
+	
+	private void cancelar() {
+		
+	}
+
+
+	@Override
+	public void actualizar(Evento e, Object datos) {
+		// TODO Auto-generated method stub
+		
+	}
 }
